23aa44b42b98c098342cf83aff772a189ff75a05
gucabal <cabralalvesgustavo@gmail.com>
2024-01-23 18:46:25 -0300

    Criando tratativas de erros e mensagens

--- src/main/java/br/com/njImports/controller/UsuarioController.java
+++ src/main/java/br/com/njImports/controller/UsuarioController.java
@@ -2,12 +2,18 @@ package br.com.njImports.controller;
 import br.com.njImports.Repository.IUsuario;
 import br.com.njImports.model.Usuarios;
 import br.com.njImports.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.yaml.snakeyaml.events.Event;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -28,21 +34,34 @@ public class UsuarioController {
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity cadastrarUsuario(@RequestBody Usuarios usuario){
+    public ResponseEntity cadastrarUsuario(@Valid @RequestBody Usuarios usuario){
         return ResponseEntity.status(201).body(usuarioService.cadastrarUsuario(usuario));
     }
 
     @PutMapping("/alterar")
-    public ResponseEntity alterarUsuario(@RequestBody Usuarios usuario){
+    public ResponseEntity alterarUsuario(@Valid @RequestBody Usuarios usuario){
         return ResponseEntity.status(200).body(usuarioService.alterarUsuario(usuario));
     }
 
 
     @PostMapping("/login")
-    public ResponseEntity loginUsuario(@RequestBody Usuarios usuario){
+    public ResponseEntity loginUsuario(@Valid @RequestBody Usuarios usuario){
         ResponseEntity<Object> valido = usuarioService.validarSenha(usuario);
         return valido;
 
+
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
 
     }
 
